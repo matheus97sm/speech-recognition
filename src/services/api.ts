9587acabcf64799e62524b6ea1f077c850d49c1d
Matheus Fonseca <matheus97sm@gmail.com>
2021-04-02 11:18:08 -0300

    Added https to wikipedia Api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: `http://en.wikipedia.org/w/`,
+  baseURL: `https://en.wikipedia.org/w/`,
   headers: {
     'Content-Type': 'application/json'
   }
